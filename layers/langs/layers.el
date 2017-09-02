8276d4f346ed2eb6f6a50183de5bb3eab6c598a7
Juan Edi <juan@noredink.com>
2017-09-02 13:16:36 -0300

    Created langs layer

---
+++ layers/langs/layers.el
@@ -0,0 +1,43 @@
+(configuration-layer/declare-layers
+ '(
+   react
+   haskell
+   erlang
+   python
+   yaml
+   clojure
+   elixir
+   latex
+   purescript
+   vimscript
+   csv
+
+   (ruby
+    :variables
+    ruby-insert-encoding-magic-comment nil
+    ruby-test-runner 'rspec
+    ruby-version-manager 'chruby
+    )
+
+   (elm
+    :variables
+    elm-format-on-save t
+    elm-sort-imports-on-save t
+    )
+
+   (javascript
+    :variables
+    js-indent-level 2
+    js2-strict-missing-semi-warning nil
+    js2-basic-offset 2
+    )
+
+   (html
+    :variables
+    css-indent-offset 2
+    web-mode-markup-indent-offset 2
+    web-mode-css-indent-offset 2
+    web-mode-code-indent-offset 2
+    web-mode-attr-indent-offset 2
+    )
+   ))
---
+++ layers/langs/packages.el
@@ -0,0 +1,18 @@
+;;; packages.el --- langs layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Juan Edi <jedi@chappie.local>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Code:
+
+(defconst langs-packages
+  '()
+)
+
+;;; packages.el ends here

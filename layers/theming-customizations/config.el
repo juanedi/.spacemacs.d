1a8d0524bac493dc5c1aefd204a68793bcc3ab22
Juan Edi <juan@noredink.com>
2017-09-17 20:45:28 -0300

    Theming customization: popups

--- layers/theming-customizations/config.el
+++ layers/theming-customizations/config.el
@@ -1,15 +1,14 @@
 (setq theming-modifications
       '(
         (spacegray
-         (popup-tip-face :inherit 'highlight)
-         (popup-tip-face :background "#232830")
+         (popup-tip-face :inherit 'flycheck-error-list-highlight)
          )
         (sanityinc-tomorrow-night
          (popup-tip-face :inherit 'highlight)
          (fringe :background "#1d1f21")
          )
         (sanityinc-tomorrow-day
-         (popup-tip-face :inherit 'highlight)
+         (popup-tip-face :background "grey20" :foreground "white")
          (fringe :background "#ffffff")
          )
         ))

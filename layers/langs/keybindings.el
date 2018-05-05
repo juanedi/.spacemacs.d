2cc83cf1d80b914c907a072de8416d138eea5b3d
Juan Edi <juan@noredink.com>
2018-05-05 12:15:00 -0300

    better keybindings for elm imports

--- layers/langs/keybindings.el
+++ layers/langs/keybindings.el
@@ -1,7 +1,7 @@
 (spacemacs/set-leader-keys-for-major-mode 'ruby-mode "t B" 'jedi/rspec-verify-fdoc)
 
-(spacemacs/set-leader-keys-for-major-mode 'elm-mode "m i" 'jedi/elm-import)
-(spacemacs/set-leader-keys-for-major-mode 'elm-mode "m f i" 'jedi/elm-import-from-file)
+(spacemacs/set-leader-keys-for-major-mode 'elm-mode "m i i" 'jedi/elm-import)
+(spacemacs/set-leader-keys-for-major-mode 'elm-mode "m i f" 'jedi/elm-import-from-file)
 (spacemacs/set-leader-keys-for-major-mode 'elm-mode "m y" 'jedi/elm-show-and-copy-module-name)
 
 (spacemacs/set-leader-keys-for-major-mode 'elm-mode "t o"

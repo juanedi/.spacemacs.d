a7a595c856ec59fedbfc9975b9714b5516f9083e
Juan Edi <juan@noredink.com>
2019-03-06 11:59:23 -0300

    Disable chruby

--- layers/ruby-extra/layers.el
+++ layers/ruby-extra/layers.el
@@ -3,6 +3,10 @@
     :variables
     ruby-insert-encoding-magic-comment nil
     ruby-test-runner 'rspec
-    ruby-version-manager 'chruby
+
+    ;; always set ruby version via direnv to avoid weird race conditions between
+    ;; chruby and direnv
+    ruby-version-manager nil
+
     ruby-enable-enh-ruby-mode nil
     )))

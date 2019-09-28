8142de04724d0c71ed6ee688f10044123c7a14f0
Juan Edi <juan@noredink.com>
2019-09-28 20:33:56 -0300

    set keybindings for both ruby modes

--- layers/ruby-extra/keybindings.el
+++ layers/ruby-extra/keybindings.el
@@ -1,6 +1,10 @@
-(spacemacs/set-leader-keys-for-major-mode 'ruby-mode "t B" 'ruby-extra/rspec-verify-fdoc)
+(seq-do
+ (lambda (mode)
+   (spacemacs/set-leader-keys-for-major-mode mode "t B" 'ruby-extra/rspec-verify-fdoc)
 
-(spacemacs/set-leader-keys-for-major-mode 'ruby-mode "t o"
-  (lambda ()
-    (interactive)
-    (misc/run-in-split-window 'rspec-toggle-spec-and-target)))
+   (spacemacs/set-leader-keys-for-major-mode mode "t o"
+     (lambda ()
+       (interactive)
+       (misc/run-in-split-window 'rspec-toggle-spec-and-target)))
+   )
+ '(ruby-mode enh-ruby-mode))

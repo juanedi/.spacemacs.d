d3f51b5a12abec5605c89a543a35a924f6f1b710
Juan Edi <jedi@chappie.local>
2020-06-23 15:02:50 -0300

    don't consider '.' part of elm symbols

--- layers/elm-extra/packages.el
+++ layers/elm-extra/packages.el
@@ -18,6 +18,23 @@
   )
 
 (defun elm-extra/post-init-elm-mode ()
+  (use-package elm-mode
+    :config
+    ;; reverts the following commit that made '.' be considered part of symbols for compatibility with language servers:
+    ;; https://github.com/jcollard/elm-mode/commit/3f042ec430e7d915bfdca21d4f4d6201a882434b
+    (setq elm--syntax-table
+          (let ((st (make-syntax-table)))
+            (modify-syntax-entry ?\{  "(}1nb" st)
+            (modify-syntax-entry ?\}  "){4nb" st)
+            (modify-syntax-entry ?-  ". 123" st)
+            (modify-syntax-entry ?\n ">" st)
+            ;; THIS CHANGED!
+            ;; (modify-syntax-entry ?. "_" st)
+
+            (modify-syntax-entry ?\" "\"\"" st)
+            (modify-syntax-entry ?\\ "\\" st)
+            st)))
+
   (add-hook
    'elm-mode-hook
    (lambda ()
